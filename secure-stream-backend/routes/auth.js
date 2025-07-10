11a45f8b00df1d08a5aa293f193e337857c70dce
Puneet Kumar <puneetk49081@gmail.com>
2025-07-10 22:55:49 +0530

    sign up route created

---
+++ secure-stream-backend/routes/auth.js
@@ -0,0 +1,61 @@
+import { Router } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+const router=Router();
+
+// Sign UP route
+router.post("/signup",async(req,res)=>{
+    try {
+        const {username,email,password}=req.body;
+
+        if(!username||!password||!email){
+            return res.status(400).json({msg:"All fields are required"})
+        }
+
+        if(password.length<6){
+            return res.status(400).json({msg:"Password must be atleast 6 characters long "})
+        }
+
+        //Checking existing user 
+        const existingUser=await User.findOne({$or: [{ email }, { username }]});
+        if(existingUser){
+            return res.status(400).json({msg:"User already exists"})
+        }
+
+        //Hashing
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(password,salt);
+        
+        //Save user
+        const newUser=new User({username,email,password:hashedPassword});
+        await newUser.save();
+        res.status(201).json({msg:"Signup successful"});
+
+
+    } catch (error) {
+        res.sendStatus(500).json({msg:"Signup error",error:err.message});
+    }
+});
+
+
+//Login route
+router.post("/login",async(req,res)=>{
+    try {
+        const{email,password}=req.body;
+        if(!email||!password){
+            return res.status(400).json({msg:"Email and password are required"});
+        }
+
+        //find user
+        const user=User.findOne({email});
+        if(!user)
+
+
+
+
+
+    } catch (error) {
+        
+    }
+})
